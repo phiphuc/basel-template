78b96eec3bbe301b82889f29c349e3523b9fd851
donqu <itsol>
2019-08-15 23:48:03 +0700

    first"

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name baselApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the baselApp
+ */
+angular.module('baselApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
